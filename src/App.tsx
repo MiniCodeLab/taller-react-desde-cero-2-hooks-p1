325a3e0ddce30319422add5bf1f28f5bdd68f147
Ccastillo06 <ccastillon92@gmail.com>
2022-02-11 09:33:26 +0100

    feat: finish workshop but pending exercises docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react'
+// React.useState
+// React.useEffect
 import './App.css';
+import MiniCodeComplexState from './components/MiniCodeComplexState';
+import MiniCodeEffectMount from './components/MiniCodeEffectMount';
+import MiniCodeEffectUnmount from './components/MiniCodeEffectUnmount';
+import MiniCodeInput from './components/MiniCodeInput';
+import MiniCodeStateCallback from './components/MiniCodeStateCallback';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Ejemplos con useState</h2>
+
+      <MiniCodeInput />
+
+      <hr />
+      <hr />
+
+      <MiniCodeComplexState />
+
+      <hr />
+      <hr />
+
+      <MiniCodeStateCallback />
+
+      <hr />
+
+      <h2>Ejemplos con useEffect</h2>
+
+      <MiniCodeEffectMount />
+
+      <hr />
+      <hr />
+
+      <MiniCodeEffectUnmount />
     </div>
   );
-}
+};
 
 export default App;
